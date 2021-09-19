0c7522df58e67665154d80895a45cade880d956a
Sovazavr <vadimsopov00@mail.ru>
2021-09-19 15:39:34 +0700

    verstka

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+//"hoist-non-react-statics/node_modules/@types/react";
+import { useQuery } from "react-query";
+//components
+import Item from "./item/item";
+import Drawer from "@material-ui/core/Drawer"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import Grid from "@material-ui/core/Grid"
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart"
+import Badge from "@material-ui/core/Badge"
+//styles
+import { Wrapper } from "./App.styles";
+import { CartItemType } from "./type";
 
-function App() {
+
+const getProducts = async ():Promise<CartItemType[]> => 
+  await(await fetch('http://fakestoreapi.com/products')).json()
+
+
+const App = () => {
+  const {data, isLoading, error}=useQuery<CartItemType[]>('products',getProducts)
+
+  const getTotalItems=()=>null
+
+  const handleAddToCart=()=>null
+
+  const handleRemoveFromCart=()=>null
+
+  if(isLoading) return <CircularProgress />
+  if(error) return <div>Ошибка, попробуйте выйти в окно</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item=>(
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
